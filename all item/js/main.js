dd772b139958497be3aa63636f6054c6ddd33f6a
Brijesh_chauhan <74099006+Brijesh-18@users.noreply.github.com>
2021-07-03 18:30:05 +0530

    Add files via upload

---
+++ all item/js/main.js
@@ -0,0 +1,78 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '40px',
+    duration: 700,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
+
+// =====Form API ======
+
+function form() {
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let massage = document.querySelector('#massage').value;                        
+    let encodedMassage = encodeURI(massage)
+
+    fetch(`https://api.callmebot.com/whatsapp.php?phone=+917738436422&text=name%20%3A${name}%0Aemail%20%3A${email}%0Amassage%20%3A${encodedMassage}&apikey=354516`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+    document.querySelector('form').reset();
+    let msg = document.querySelector('#msg');
+    msg.classList.toggle('hide')
+    setTimeout(function(){msg.classList.toggle('hide')},4000)
+
+}
+
+// ======Dark mode====
+
